fc3cd9de44ad0971d8c0f9bf7ccba46cf94c9c76
joyodanjuma25 <danjumaojoy7@gmail.com>
2022-06-24 21:59:18 +0100

    about me

---
+++ output.js
@@ -0,0 +1,9 @@
+document.write('<h1>Introduction To JavaScript</h1><br>Name: Joy Onechojon Danjuma <br> Height: 5.7cm<br> Country: Nigeria');
+
+const myName = 'Joy Onechojon Danjuma';
+const myHeight = '5.7cm tall';
+const myCountry = 'Nigeria';
+
+console.log(`My name is ${myName} I am ${myHeight} I am from ${myCountry}`);
+
+alert('Hi my name is Joy Onechojon Danjuma, am 5.7cm tall and am from nigeria');
